288dcbe3149e5d95552c6a5003825c66864ce5c5
AmplifiedInnovators <sihlemkhatshwa04@gmail.com>
2024-11-12 21:34:27 +0200

    Year and edit page
    -Added the edit page
    -added the js file
    -renamed the folders

---
+++ js/Books.js
@@ -0,0 +1,7 @@
+function DeleteBook() {
+    const message = "Are you sure you want to delete the book?";
+    if (confirm(message)) {
+        // Action to take if confirmed
+        alert("Book Deleted");
+    }
+}
